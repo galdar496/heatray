37071dd4546d3a86155e8713ce9dc5a0a554e07e
galdar496 <galdar496@gmail.com>
2021-10-10 17:15:34 -0700

    Consolidate vertex shaders.

--- Source/HeatrayRenderer/Materials/Material.h
+++ Source/HeatrayRenderer/Materials/Material.h
@@ -36,7 +36,7 @@ public:
 
 	Type type() const { return m_type; }
 protected:
-    static constexpr char const * m_vertexShader = "positionNormal.vert.rlsl";
+    static constexpr char const * m_vertexShader = "vertex.rlsl";
 
     std::shared_ptr<openrl::Buffer>  m_constants = nullptr; ///< Constants used by this material. Will be uploaded as a uniform block to the corresponding shader.
     std::shared_ptr<openrl::Program> m_program   = nullptr; ///< Shader representing this material.
