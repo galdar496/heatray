82ae375112c99a3aa0f76a17a6baf01a0aaa559f
Cody White <cody@viewmagic.com>
2021-12-13 16:21:13 -0800

    Implement a macOS save dialog.

--- Source/Utility/FileDialogMacOS.mm
+++ Source/Utility/FileDialogMacOS.mm
@@ -11,12 +11,10 @@ std::vector<std::string> OpenFileDialog(const std::string &extension)
     
     NSInteger buttonPressed = [openPanel runModal];
     
-    if (buttonPressed == NSModalResponseOK)
-    {
+    if (buttonPressed == NSModalResponseOK) {
         std::vector<std::string> retVal;
 
-        for (int i = 0; i < [[openPanel URLs] count]; ++i)
-        {
+        for (int i = 0; i < [[openPanel URLs] count]; ++i) {
             NSURL * selectedFile = [[openPanel URLs] objectAtIndex:i];
 
             retVal.push_back([[selectedFile path] UTF8String]);
@@ -30,6 +28,18 @@ std::vector<std::string> OpenFileDialog(const std::string &extension)
 
 std::vector<std::string> SaveFileDialog(const std::string &extension)
 {
+    NSSavePanel * savePanel = [NSSavePanel savePanel];
+    
+    NSInteger buttonPressed = [savePanel runModal];
+    
+    if (buttonPressed == NSModalResponseOK) {
+        std::vector<std::string> retVal;
+
+        NSURL * selectedFile = [savePanel URL];
+        retVal.push_back([[selectedFile path] UTF8String] + std::string(".") + extension);
+        return retVal;
+    }
+
     return std::vector<std::string>();
 }
 
